73374c2b1da22b8b238866d0706ee8f9081f9a8f
deissypc <deissyparrac@gmail.com>
2022-10-20 20:42:48 -0500

    api3

---
+++ evidencias/callback/peces.js
@@ -0,0 +1,39 @@
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+let endpoint = 'https://fakestoreapi.com/products'
+
+const exito = (response)=>{
+    const tipos = JSON.parse(response).results
+    tipos.forEach((tipo)=>{
+        console.log(`Tipo:${tipo.title}`)
+        console.log(`-----------------`)
+    })
+    
+/*console.log(JSON.parse(response).results*/
+}
+
+const fallo =(status)=>{
+    console.log(status)
+}
+
+
+const get_data=(endpoint, exito, fallo)=>{
+    //CREAMOS EL OBJETO DE CONEXION A LA API
+let http=new XMLHttpRequest()
+//abrir una conexion
+http.open('get',endpoint)
+//enviar la solicitud(request) a la api
+http.send()
+//hacer el tratamiento de la response
+http.onload = ()=>{
+    if(http.status===200){
+        exito(http.responseText)
+    }else{
+        fallo(http.status)
+    }
+
+}
+}
+
+//invocar funcion get_date
+get_data(endpoint,exito,fallo)
